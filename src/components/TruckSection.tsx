a050768239d2204b30d0a61ae688d19be0964e58
Andrew Z <zhangandrew2@gmail.com>
2025-08-16 21:52:46 -0500

    updated trucking animation scaling

--- src/components/TruckSection.tsx
+++ src/components/TruckSection.tsx
@@ -2,18 +2,14 @@ import React from "react";
 
 const TruckSection = () => {
   return (
-    <section className="bg-hemut-navyblue py-8 md:py-12">
-      <div className="container-custom">
-        <div className="mx-auto max-w-5xl">
-          <div className="aspect-[1200/260] w-full">
-            <object
-              data="/hemut-truck.svg"
-              type="image/svg+xml"
-              aria-label="Animated truck"
-              className="w-full h-full"
-            />
-          </div>
-        </div>
+    <section className="bg-hemut-navyblue py-8 md:py-12 w-screen mx-[calc(50%-50vw)]">
+      <div className="w-screen flex justify-center">
+        <object
+          data="/hemut-truck.svg"
+          type="image/svg+xml"
+          aria-label="Animated truck"
+          className="block h-auto w-screen origin-center scale-[0.8] md:scale-[0.9] lg:scale-[0.75] xl:scale-[0.7] 2xl:scale-[0.65]"
+        />
       </div>
     </section>
   );
